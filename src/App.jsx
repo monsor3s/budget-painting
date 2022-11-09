77048b22a5139d1d776236ced9d00867f3f043e5
Marlon Monsores <64220885+monsor3s@users.noreply.github.com>
2022-11-09 20:11:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import InitialPage from './pages/InitialPage'
+import './styles/reset.css'
+
+function App() {
+
+  return (
+    <div id="root">
+      <InitialPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/InitialPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header';
+import '../../styles/initialPage.css'
+
+function InitialPage() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  )
+}
+
+export default InitialPage;
